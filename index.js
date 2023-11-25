866566c611a8227a97ab3d2ab8646431b4f4af16
Pornima_Malbhage <malbhage.pornima15@gmail.com>
2023-11-25 19:39:46 +0530

    my project

---
+++ index.js
@@ -0,0 +1,174 @@
+const receipes=[
+    {
+        "name": "Veggie Delight",
+        "imageSrc": "https://source.unsplash.com/random?veggies",
+        "time": "30 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.2
+    },
+    {
+        "name": "Chicken Grill",
+        "imageSrc": "https://source.unsplash.com/random?chicken",
+        "time": "45 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.5
+    },
+    {
+        "name": "Cheese Pizza",
+        "imageSrc": "https://source.unsplash.com/random?pizza",
+        "time": "40 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.1
+    },
+    {
+        "name": "Steak",
+        "imageSrc": "https://source.unsplash.com/random?steak",
+        "time": "60 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.7
+    },
+    {
+        "name": "Grilled Salmon",
+        "imageSrc": "https://source.unsplash.com/random?salmon",
+        "time": "50 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.6
+    },
+    {
+        "name": "Tomato Pasta",
+        "imageSrc": "https://source.unsplash.com/random?pasta",
+        "time": "35 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.0
+    },
+    {
+        "name": "Vegan Salad",
+        "imageSrc": "https://source.unsplash.com/random?salad",
+        "time": "20 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 3.9
+    },
+    {
+        "name": "Fried Chicken",
+        "imageSrc": "https://source.unsplash.com/random?friedChicken",
+        "time": "55 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.3
+    },
+    {
+        "name": "Mushroom Risotto",
+        "imageSrc": "https://source.unsplash.com/random?risotto",
+        "time": "45 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.5
+    },
+    {
+        "name": "Burger",
+        "imageSrc": "https://source.unsplash.com/random?burger",
+        "time": "30 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.2
+    },
+    {
+        "name": "Paneer Tikka",
+        "imageSrc": "https://source.unsplash.com/random?paneerTikka",
+        "time": "40 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.4
+    },
+    {
+        "name": "BBQ Ribs",
+        "imageSrc": "https://source.unsplash.com/random?ribs",
+        "time": "70 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.6
+    },
+    {
+        "name": "Caesar Salad",
+        "imageSrc": "https://source.unsplash.com/random?caesarSalad",
+        "time": "25 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 3.8
+    },
+    {
+        "name": "Fish Tacos",
+        "imageSrc": "https://source.unsplash.com/random?fishTacos",
+        "time": "35 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.3
+    },
+    {
+        "name": "Chocolate Cake",
+        "imageSrc": "https://source.unsplash.com/random?chocolateCake",
+        "time": "90 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.9
+    }
+]
+
+
+const searchInput = document.querySelector('.search_input')
+
+searchInput.addEventListener("input", (e) => {
+   
+    let value = e.target.value
+
+    
+    if (value && value.trim().length > 0){
+      
+         value = value.trim().toLowerCase()
+       
+    } else {
+        
+
+    }
+
+})
+
+function setList(results){
+
+for (const receipes of results){
+    // creating a li element for each result item
+    const resultItem = document.createElement('li')
+
+    // adding a class to each item of the results
+    resultItem.classList.add('result-item')
+
+    // grabbing the name of the current point of the loop and adding the name as the list item's text
+    const text = document.createTextNode(receipes.name)
+
+    // appending the text to the result item
+    resultItem.appendChild(text)
+
+    // appending the result item to the list
+    list.appendChild(resultItem)
+}
+}
+
+searchInput.addEventListener("input", (e) => {
+    let value = e.target.value
+
+    if (value && value.trim().length > 0){
+         value = value.trim().toLowerCase()
+
+        //returning only the results of setList if the value of the search is included in the person's name
+        setList(people.filter(person => {
+            return person.name.includes(value)
+        } ) )
+
+    }})
